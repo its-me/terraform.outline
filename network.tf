aeca76e3edd8a1dc0e8850ff73ee707b63f73985
Sergey Kanafyev <sergeykanafyev@gmail.com>
2026-08-09 17:58:36 +0800

    Source shared network module from GitHub instead of a local path

--- network.tf
+++ network.tf
@@ -23,7 +23,7 @@ resource "google_project_service" "apis" {
 # terraform.twenty is the owning caller (create = true); this is a consuming caller
 # (create = false) that reads the same `name` back instead of creating its own.
 module "network" {
-  source = "../terraform.module.network"
+  source = "git::https://github.com/its-me/terraform.module.network.git"
 
   project_id = var.project_id
   region     = var.region
